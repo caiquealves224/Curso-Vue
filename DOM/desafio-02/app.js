7b7d7679e1ad36903d509bf034273c60ef3ac2d1
Caique Alves dos Santos <caiquealves224@gmail.com>
2019-01-09 13:26:37 -0200

    resolvendo desafio

--- DOM/desafio-02/app.js
+++ DOM/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods : {
+        alertar(){
+            alert("Uahauuu")
+        },
+        setValor(){
+            this.valor = event.target.value;
+        }
     }
 })
